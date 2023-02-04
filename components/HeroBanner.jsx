6c345d7fe609efaeefd218814d269efc3f1348d2
irazvanbotezatu <rezzy_botez@yahoo.com>
2023-02-04 09:48:55 +0200

    fv2

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -16,7 +16,7 @@ const HeroBanner = ({ heroBanner }) => {
         />
 
         <div>
-          <Link href="/product/ID">
+          <Link href="">
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
